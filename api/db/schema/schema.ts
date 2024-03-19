473250da379e85111452decb673e494bf033c01a
Dejan <dejan@Dejans-MacBook-Pro.local>
2024-03-19 16:56:10 +0100

    REST api init - comment delete

--- api/db/schema/schema.ts
+++ api/db/schema/schema.ts
@@ -1,14 +1,3 @@
-// import { index, int, mysqlTable, bigint, varchar } from 'drizzle-orm/mysql-core';
-
-// export const users = mysqlTable('users', {
-//   id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
-//   name: varchar('name', { length: 256 }),
-//   email: varchar("email", {length: 256}),
-//   password: varchar("password", {length:256}),
-// }, (users) => ({
-//   nameIdx: index('name_idx').on(users.name),
-// }));
-
 import {users} from "./users"
 
 export {users};
