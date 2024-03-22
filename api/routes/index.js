c961713a1c40b3b9eabcde696419eab6e2ef010b
Dejan <dejan@Dejans-MacBook-Pro.local>
2024-03-22 15:58:44 +0100

    switch to ES6 syntax

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import {db} from '../db/db.js';
+import {users} from '../db/schema/users.js';
+
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
+  const result = await db.select().from(users);
+  console.log(result);
   res.status(200).send("Hello world");
 });
 
-module.exports = router;
+export default router;
