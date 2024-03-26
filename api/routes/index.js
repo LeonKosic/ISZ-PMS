b99edaae75c3d0e6421496ddceef39382e0d88b2
Dejan <dejan@Dejans-MacBook-Pro.local>
2024-03-26 20:53:43 +0100

    Cors and body parser

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {db} from '../db/db.js';
 import {users} from '../db/schema/users.js';
+import bodyParser from 'body-parser';
 
 
 const router = express.Router();
+const jsonParser = bodyParser.json()
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -11,5 +13,12 @@ router.get('/', async (req, res, next) => {
   console.log(result);
   res.status(200).send("Hello world");
 });
+router.post("/", jsonParser, async (req, res, next)=>{
+  console.log(req.body)
+  if(req.body.idk == "hello"){
+    res.status(200).send("hello world")
+  }
+  res.status("203").send("now good g")
+})
 
 export default router;
