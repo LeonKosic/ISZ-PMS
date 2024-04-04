fa2b8355300759c5ad24fe54da9c76491273c235
ognjen <ognjen.komadina@student.etf.unibl.org>
2024-04-04 12:46:58 +0200

    .

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,17 +3,12 @@ import ProfileHeader from "~/components/ProfileHeader";
 import ProjectBanner from "~/components/ProfileProjectBanner";
 import ProfileRepositoryBanner from "~/components/ProfileRepositoryCard";
 
+import api from "~/api/api";
+
 export default function Profile(props) {
-  const redirectRepository = (url) => {
-    return;
-  } 
-  
   return (
     <div class="user-profile">
       <div class="activities">
-        <ProfileActivityCard />
-        <ProfileActivityCard />
-        <ProfileActivityCard />
         <ProfileActivityCard />
       </div>
       
@@ -22,39 +17,20 @@ export default function Profile(props) {
         
         <div class="social">
           <div class="following">
-            Following: <br/>
-            Jane Doe <br/>
-            Joe Biden <br/>
-            Joe Biden <br />
-            Joe Biden <br/>
           </div>
           <div class="popular-projects">
-            Most popular projects: <br/>
-            ProjectA<br/>
-            ProjectB<br/>
-            ProjectC<br/>
           </div>
         </div>
         
         <div class="project-listings">
           <ProjectBanner />
-          <ProjectBanner />
-          <ProjectBanner />
-          <ProjectBanner />
         </div>
       </div>
       
       <div class="repositories">
+        <h1 class="text-huge">Spotlight</h1>
         {/* grabe se repozitorijumi i prikazuju u <For> komponenti (sa limitom, tipa top 3) */}
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
-        <ProfileRepositoryBanner />
+        <ProfileRepositoryBanner/>
       </div>
     </div>
   )
