49a01d92e7f46369e280a0b958d44c95f78bb7d2
leon <leon@bitlab.rs>
2024-04-09 15:23:43 +0200

    adding gin

--- filesystem/main.go
+++ filesystem/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("testtt")
+	router := gin.Default()
+	router.GET("/", response)
+	router.Run()
+}
+
+func response(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "test",
+	})
 }
