d33fee522223b89f273bcf97f78836d7d49c00c2
leon <leon@bitlab.rs>
2024-04-10 13:32:27 +0200

    small fixes

--- filesystem/internal/server/server.go
+++ filesystem/internal/server/server.go
@@ -8,17 +8,24 @@ import (
 	"github.com/rs/cors"
 )
 
-func RunServer(api string, port string) {
+func RunServer() {
 	cfg := config.DefaultConfig()
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "TEST")
-	})
-	c := cors.New(cors.Options{
+	router(mux)
+	handler := cors.New(cors.Options{
 		AllowedOrigins:   []string{cfg.Host},
 		AllowCredentials: true,
 		Debug:            true,
-	})
-	handler := c.Handler(mux)
+	}).Handler(mux)
 	http.ListenAndServe(cfg.Port, handler)
+}
+
+func router(mux *http.ServeMux) {
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "TEST")
+	})
+	mux.HandleFunc("GET /project/{id}", func(w http.ResponseWriter, r *http.Request) {
+		projectID := r.PathValue("id")
+		fmt.Fprintf(w, projectID)
+	})
 }
