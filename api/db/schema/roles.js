f3da3a8f0a08f895ff64faa98c2a84f826b3dc7d
Maja Tripunovic <tripunovicmaja@gmail.com>
2024-04-17 18:46:01 +0200

    category table,create,delete

--- api/db/schema/roles.js
+++ api/db/schema/roles.js
@@ -2,5 +2,6 @@ import {  mysqlTable, bigint,varchar } from 'drizzle-orm/mysql-core';
 
 export const roles= mysqlTable('role',{
     role_id: bigint('role_id', { mode: 'number' }).primaryKey().autoincrement(),
-    role_name:varchar('role_name', { length: 256 })
+    role_name:varchar('role_name', { length: 256 }),
+    deleted:
 })
--- api/db/schema/schema.js
+++ api/db/schema/schema.js
@@ -1,11 +1,13 @@
 import {users} from "./users.js"
 import {follow} from "./follow.js"
 import {roles} from "./roles.js"
+import { category } from "./category.js";
 import relations from "./relations/relations.js";
 import { userRelations } from "./relations/userRelations.js";
 import { followRelations } from "./relations/followRelations.js";
 import { roleRelations } from "./relations/roleRelations.js";
 
 
-export {users, follow, roles, userRelations, followRelations, roleRelations}
-export default{users, follow, roles, userRelations, followRelations, roleRelations}
+export {users, follow, roles,category, userRelations, followRelations, roleRelations}
+export default{users, follow, roles,category, userRelations, followRelations, roleRelations}
+
