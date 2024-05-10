d991539b4ddb32be5d6789c0dc1d4d7c579e929b
ognjen <ognjen.komadina@student.etf.unibl.org>
2024-05-10 19:36:16 +0200

    feat: suid palette, fix: tailwind extends

---
+++ frontend/src/styles/suidTheme.jsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@suid/material";
+
+const theme = createTheme({
+  palette: {
+    pmsScheme: {
+      main: '#e7e2e2',
+      light: '#fefefe',
+      dark: '#371a1a',
+      contrastText: '#fefefe'
+    }
+  }
+})
+
+export default theme;
+
+// import theme from ".../suidTheme.jsx"
+// <Button color={theme} />
+// ima detaljnije na mui dokumentaciji
