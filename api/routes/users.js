fa869e6fd89eab93728f6e5137c9a169de16a07e
dejan <dekivuk00@gmail.com>
2024-05-11 10:14:35 +0200

    search users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,5 @@
 import {users} from "../db/schema/users.js"
-import { Name,eq } from 'drizzle-orm';
+import { Name,eq, like } from 'drizzle-orm';
 import {db} from '../db/db.js';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -136,6 +136,19 @@ router.get('/details',jsonParser,async(req,res)=>{
   res.send(400,{err:"Username does not exist."})
     return
 
+})
+router.post('/search', jsonParser ,async(req,res)=>{
+  console.log(req.body)
+  const existingUser = await db.select().from(users).where(like(users.user_name,`%${req.body.user_name}%`) && eq(users.deleted,0))
+  
+  if(existingUser.length>0){
+    existingUser.map((user)=>{
+      delete user.password
+    })
+    return res.send(200,existingUser)
+  }
+  res.send(400,req.body)
+    return
 })
 
 export default router;
