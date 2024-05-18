a2e4b5bd050f4e94ac4d5984ed0e83e5cab35cec
Strahinja Maksimovic <strahiboss@gmail.com>
2024-05-18 17:05:19 +0200

    Dodane PostCard i PostList komponente
    WIP, stari landing rastavljen na komponente

---
+++ frontend/src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import {For} from "solid-js"
+import PostCard from "../components/PostCard"
+
+export default function PostList(props){
+    const posts = props.data
+    console.log(posts)
+    return(
+        <div class="grid grid-cols-1 gap-3 bg-primary-900 mt-3 h-full">
+          <For each={posts}>{(post) =>(
+            <PostCard data={post}/>
+          )}</For>
+        </div>
+    )
+}
