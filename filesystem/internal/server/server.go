e6947aaeca834659da6a4409cfdcc55112a5d129
LeonKosic <leonkosic@protonmail.com>
2024-05-28 11:57:54 +0200

    temp uploader

--- filesystem/internal/server/server.go
+++ filesystem/internal/server/server.go
@@ -8,17 +8,31 @@ import (
 	"github.com/rs/cors"
 )
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(5 << 20) //5MB
+	multipartFormData := r.MultipartForm
+
+	for _, v := range multipartFormData.File["attachments"] {
+		fmt.Println(v.Filename, ":", v.Size)
+		uploadedFile, _ := v.Open()
+		uploadedFile.Read([]byte{})
+		uploadedFile.Close()
+	}
+}
+
 func RunServer(api string, port string) {
 	cfg := config.DefaultConfig()
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "TEST")
 	})
+	mux.HandleFunc("POST /upload/", uploadFile)
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{cfg.Host},
 		AllowCredentials: true,
 		Debug:            true,
 	})
+
 	handler := c.Handler(mux)
 	http.ListenAndServe(cfg.Port, handler)
 }
