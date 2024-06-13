107fab83b2d7207119dac29a10bdd1da41bb4529
LeonKosic <leonkosic@protonmail.com>
2024-06-13 10:22:49 +0200

    typo fix

--- filesystem/internal/config/config.go
+++ filesystem/internal/config/config.go
@@ -1,17 +1,17 @@
 package config
 
 type Config struct {
-	Host string
-	Port string
-	Dir  string
+	Host   string
+	Port   string
+	Dir    string
 	Commit string
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		Host: "localhost",
-		Port: ":7070",
-		Dir:  "files/",
-		Commit: "commits/"
+		Host:   "localhost",
+		Port:   ":7070",
+		Dir:    "files/",
+		Commit: "commits/",
 	}
 }
