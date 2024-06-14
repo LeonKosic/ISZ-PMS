4ca90c418c325b79bdff465d0e074b7638935a5b
ognjen <ognjen.komadina@student.etf.unibl.org>
2024-06-14 02:43:51 +0200

    footer fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,20 +1,24 @@
 
 export default function Navbar(props) {
-  
+
   return (
-    <div class="absolute bottom-0 bg-primary-800 pb-0 z-50">
-      <hr class="mt-4 pb-0 w-full text-accent-700" />
-        <div class="grid grid-rows-1 grid-cols-3 text-center">
-            <div>
-                <p>© PMS 2024 University of Banja Luka Faculty of Electrical Engineering</p>
-            </div>
-            <div>
-                <button onClick = {() => {window.location.href = "/report"}}>Report a problem</button>
-            </div>
-            <div>
-                <button onClick = {() => {window.location.href = "/about"}}>About us</button>
-            </div>
+    <div class="absolute bottom-0 bg-primary-800 pb-0 z-50 w-full">
+      <hr class="mt-4 pb-0 text-accent-700" />
+      <div class="grid grid-rows-1 grid-cols-3 text-center items-center align-middle italic">
+        <div>
+          <p>
+            © PMS 2024 <br />
+            University of Banja Luka <br />
+            Faculty of Electrical Engineering
+          </p>
         </div>
+        <div class="hover:underline transition-all duration-300">
+          <button onClick={() => { window.location.href = "/report" }}>Report a problem</button>
+        </div>
+        <div class="hover:underline transition-all duration-300">
+          <button onClick={() => { window.location.href = "/about" }}>Meet the team</button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,14 +3,9 @@ export default function About(props){
 
     return (
       <div class="grid grid-cols-1 gap-3 gap-y-3 my-3 justify-center text-center place-items-center object-top h-5/6">
-          <h2 class="text-2xl py-3 my-3 font-semibold text-gray-50">About us</h2>
+          <h2 class="text-2xl py-3 my-3 font-semibold text-gray-50">Meet the team</h2>
           <div>
             <img class="py-3" src="../src/assets/team.jpg"/>
-          </div>
-          <div>
-            <p>We are a team of 6 software engineering students at the Faculty of Electrical engineering, University of Banja Luka.
-            This web application is our project for the subject Software design.
-            </p>
           </div>
         </div>   
     )
